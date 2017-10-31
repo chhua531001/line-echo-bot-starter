b9f099c15b7849d7f61631b586cee275e1ed7044
chhua531001 <chhua531001@gmail.com>
2017-10-31 13:20:27 +0800

    add hashmap code

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 let express = require('express')
 let bodyParser = require('body-parser')
 let request = require('request')
+let HashMap = require('hashmap')
 let app = express()
+
+var map = new HashMap();
+
+map.set("Hi", "Hi")
+.set("你好", "你好")
+.set("Hello", "Hello")
+.set("晚餐", "牛肉麵")
+.set("名字", "花志雄")
+.set("信箱", "chhua531001@gmail.com")
+
+map.forEach(function(value, key) {
+    console.log(key + " : " + value);
+});
+
 
 const CHANNEL_ACCESS_TOKEN = 'fzGSull4pgOP7HmyZ8IPeapC+JlTWP7fSFeExfgMLqrKZ67khYllJolr1jpNx+QZ+V1jSx3jYKRyokmdK43c4ZJiYYQm34yNlJDm+MJQsxC+cXU7loiBEJiROoktWBXre+99lIcQMCBc8bObFAJhiwdB04t89/1O/w1cDnyilFU='
 const PORT = process.env.PORT || 3000
@@ -21,7 +36,30 @@ app.post('/', function (req, res) {
         let replyToken = event.replyToken
         let type = event.message.type
         if(type == 'text') {
-            let text = event.message.text
+            var text = event.message.text
+
+            map.forEach(function(value, key) {
+
+                if(text.indexOf(key) != -1) {
+                    text = value
+                }                
+                // console.log(key + " : " + value);
+            });
+
+            // if(text.indexOf('名字') != -1) {
+            //     // text = '花志雄'
+            //     text = map.get("名字")
+            // }
+            // else if(text.indexOf('信箱') != -1) {
+            //     text = "chhua531001@gmail.com"
+            // }
+
+            // if(text == '你叫什麼名字') {
+            //     text = '花志雄'
+            // } 
+            // else if (text == '你的信箱') {
+            //     text = "chhua531001@gmail.com"
+            // }
             sendMessage(replyToken, text)
         } 
         else {
